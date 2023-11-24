f6a41eef88fce0f7879534bc3a1513922ed4b0e8
Zdeněk Vydra <485514@mail.muni.cz>
2023-11-24 17:33:34 +0100

    Document Client struct

--- pkg/kypo/client.go
+++ pkg/kypo/client.go
@@ -8,14 +8,31 @@ import (
 // Client struct stores information for authentication to the KYPO API.
 // All functions are methods of this struct
 type Client struct {
-	Endpoint        string
-	ClientID        string
-	HTTPClient      *http.Client
-	Token           string
+	// Endpoint of the KYPO instance to connect to. For example `https://your.kypo.ex`.
+	Endpoint string
+
+	// ClientID used by the KYPO instance OIDC provider.
+	ClientID string
+
+	// HTTPClient which is used to do requests.
+	HTTPClient *http.Client
+
+	// Bearer Token which is used for authentication to the KYPO instance. Is set by NewClient function.
+	Token string
+
+	// Time when Token expires, used to refresh it automatically when required. Is set by NewClient function.
+	// Is used only with KYPO instances using Keycloak OIDC provider.
 	TokenExpiryTime time.Time
-	Username        string
-	Password        string
-	RetryCount      int
+
+	// Username of the user to login as.
+	Username string
+
+	// Password of the user to login as.
+	Password string
+
+	// How many times should a failed HTTP request be retried. There is a delay of 100ms before the first retry.
+	// The delay is doubled before each following retry.
+	RetryCount int
 }
 
 // NewClientWithToken creates and returns a Client which uses an already created Bearer token.
